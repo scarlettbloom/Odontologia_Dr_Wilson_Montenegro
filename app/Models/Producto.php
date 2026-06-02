f2e8c5c3d7518422e94c10e14a38da276e128a45
juansiaucho3311a-droid <juansiaucho3311a@gmail.com>
2026-06-01 19:03:28 -0500

    Add files via upload

---
+++ app/Models/Producto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    protected $table = 'producto';
+
+    protected $primaryKey = 'idproducto';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre',
+        'marca',
+        'precio',
+        'idproveedor',
+        'idadmin'
+    ];
+}
